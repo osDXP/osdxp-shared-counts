9a34de25262155683bf5fe99570a8a49d8226100
Alexandru Nitu <alexn@crowdfavorite.com>
2019-10-24 04:04:47 +0300

    initial commit

---
+++ osdxp-shared-counts.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Plugin Name: osDXP Shared Counts
+ * Plugin URI:  https://sharedcountsplugin.com
+ * Description: Social sharing buttons that look great and keep your site loading fast.
+ * Author:      Bill Erickson & Jared Atchison - osDXP version by CrowdFavorite
+ * Version:     1.4.0-osdxp
+ *
+ * Shared Counts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Shared Counts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shared Counts. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    SharedCounts
+ * @author     Bill Erickson & Jared Atchison - osDXP version by CrowdFavorite
+ * @since      1.0.0
+ * @license    GPL-2.0+
+ * @copyright  Copyright (c) 2019
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Basic plugin constants.
+ */
+// Version.
+define( 'SHARED_COUNTS_VERSION', '1.4.0-osdxp' );
+
+// Directory path.
+define( 'SHARED_COUNTS_DIR', plugin_dir_path( __FILE__ ) );
+
+// Directory URL.
+define( 'SHARED_COUNTS_URL', plugin_dir_url( __FILE__ ) );
+
+// Update URL.
+define( 'SHARED_COUNTS_UPDATE_URL', '@TODO: ADD OSDXP.ORG URL' );
+
+// Base name.
+define( 'SHARED_COUNTS_BASE', plugin_basename( __FILE__ ) );
+
+// Plugin root file.
+define( 'SHARED_COUNTS_FILE', __FILE__ );
+
+// Plugin name.
+define( 'SHARED_COUNTS_NAME', 'osDXP Shared Counts' );
+
+// Plugin slug.
+define( 'SHARED_COUNTS_SLUG', 'osdxp-shared-counts' );
+
+/**
+ * We require a modern supported version of PHP (5.6+).
+ */
+if ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
+
+	/**
+	 * Deactivate plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	function shared_counts_deactivate() {
+
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+	}
+	add_action( 'admin_init', 'shared_counts_deactivate' );
+
+
+	/**
+	 * Display notice after deactivation.
+	 *
+	 * @since 1.0.0
+	 */
+	function shared_counts_deactivate_notice() {
+
+		echo '<div class="notice notice-warning"><p>' . esc_html__( 'Shared Counts requires PHP 5.6+. Contact your web host to update.', 'shared-counts' ) . '</p></div>';
+
+		if ( isset( $_GET['activate'] ) ) { // phpcs:ignore
+			unset( $_GET['activate'] ); // phpcs:ignore
+		}
+	}
+	add_action( 'admin_notices', 'shared_counts_deactivate_notice' );
+
+} else {
+
+	if (defined('OSDXP_DASHBOARD_VER')) {
+		// Load osDXP functionality.
+		require_once SHARED_COUNTS_DIR . 'includes/dxp.php';
+	}
+
+	/**
+	 * Load the primary.
+	 */
+	require_once SHARED_COUNTS_DIR . 'includes/class-shared-counts.php';
+
+	/**
+	 * The function provides access to the sharing methods.
+	 *
+	 * Use this function like you would a global variable, except without needing
+	 * to declare the global.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return object
+	 */
+	function shared_counts() {
+
+		return Shared_Counts::instance();
+	}
+	shared_counts();
+}
