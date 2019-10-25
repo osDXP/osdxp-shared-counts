51ccff17c42716a20b6d391000cffea5b692464a
Alexandru Nitu <alexn@crowdfavorite.com>
2019-10-25 15:21:21 +0300

    fix making sure updater class is available

--- includes/dxp.php
+++ includes/dxp.php
@@ -6,11 +6,13 @@
  */
 
 // Hook Custom Updater
-new OSDXP_Dashboard\OsdxpModuleUpdateChecker(
-    SHARED_COUNTS_UPDATE_URL,
-    SHARED_COUNTS_FILE,
-    SHARED_COUNTS_SLUG
-);
+add_action('plugins_loaded', function () {
+	new OSDXP_Dashboard\OsdxpModuleUpdateChecker(
+	    SHARED_COUNTS_UPDATE_URL,
+	    SHARED_COUNTS_FILE,
+	    SHARED_COUNTS_SLUG
+	);
+});
 
 // Change button text
 add_filter('osdxp_manage_button_module_osdxp-shared-counts', function () {
